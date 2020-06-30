78442c3e2106ce8ebb5051612ef6c0dd87fbd734
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2020-06-30 14:38:01 -0500

    ADD(): Get gifs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,59 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-     <!-- Initializing proyect -->
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <form action="" @submit.prevent="search">
+      <input type="text" v-model="q">
+      <button>search</button>
+    </form>
+    <p>{{q}}</p>
+    <section>
+      <div class="gif-container">
+          <img class="gif" v-for="result in results" :key="result.id" :src="result.images.original.url" alt="">  
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+const apiKey = "7oqbWiAr4RXO48oFiX2amhSTciHjnjY3";
+const url = "http://api.giphy.com/v1/gifs/search";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data: function() {
+    return {
+      q: "",
+      results: null,
+      limit: 10
+    };
+  },
+  methods: {
+    search() {
+      axios
+        .get(url, {
+          params: {
+            q: this.q.split(" ").join("+"),
+            apiKey: apiKey,
+            limit: this.limit
+          }
+        })
+        .then(response => {
+          console.log(this.q);
+          this.results = response.data.data;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.gif-container {
+  display: flex;
+  /* overflow: hidden; */
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.gif {
+  padding: 0.5rem;
 }
 </style>
